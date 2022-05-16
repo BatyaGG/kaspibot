5bfcd1df5842c0e92e53841c83433b885f3ffea9
BatyaGG <batyrkhan.saduanov@gmail.com>
2022-05-17 00:36:02 +0600

    asdasd

--- Customer_data/Customers.py
+++ Customer_data/Customers.py
@@ -4,4 +4,4 @@ customers[0] = {'id': 0,
                 'email': 'Kuanishbekkyzy@mail.ru',
                 'password': 'Nurislam177@',
                 'link': 'https://kaspi.kz/shop/info/merchant/11056023/address-tab/',
-                'filename': 'customer0_orders1.csv'}
+                'filename': 'customer0_orders.csv'}
